c54c0e65405306af187eddf26796e45fd98f58c8
Niladri <niladri.sinha89@gmail.com>
2021-04-27 22:11:25 -0400

    wip heroku

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function(req, res, next){
-    res.render('woodCrafts/')
+    res.render('index')
 });
 
 module.exports = router;
